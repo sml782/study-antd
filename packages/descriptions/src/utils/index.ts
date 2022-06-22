82340de305ff220f9bb617cecb1df4971e2520a2
sml782 <sml782@163.com>
2022-06-22 14:58:33 +0800

    feat(Descriptions): 实现 toArray

--- packages/descriptions/src/utils/index.ts
+++ packages/descriptions/src/utils/index.ts
@@ -1,8 +1,8 @@
 import React from 'react';
-import toArray from 'rc-util/es/Children/toArray';
-import warning from 'antd/es/_util/warning';
+// import toArray from 'rc-util/es/Children/toArray';
+import toArray from './toArray';
+import warning from '@study/util/esm/warning';
 import { responsiveArray } from '@study/util/esm/responsiveObserve';
-
 import { DEFAULT_COLUMN_MAP } from '../constants/index';
 
 import type { ScreenMap } from '@study/util/esm/responsiveObserve';
